86a972565fdc203b1ab173124b461eedbaa75782
mavelyc <cmavely@gmail.com>
2020-02-08 19:41:31 -0500

    "added .env"

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const URL = process.env.MONG_URL
-mongoose.connect(URL, {useNewUrlParser: true, useCreateIndex: true});
+const URL = process.env.MONGO_URL
+mongoose.connect(URL, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
 connection.once('open', () => {
