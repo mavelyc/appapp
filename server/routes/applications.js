a7bcc27ed4e93df3606320b9f59fd134cd64d48f
mavelyc <cmavely@gmail.com>
2020-02-09 23:21:14 -0500

    added delete

--- server/routes/applications.js
+++ server/routes/applications.js
@@ -25,4 +25,10 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').delete((req,res) => {
+    Application.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
 module.exports = router;
