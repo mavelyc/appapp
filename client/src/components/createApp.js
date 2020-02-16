55c1ae020ca843500a03635da51bfc24252a785e
mavelyc <cmavely@gmail.com>
2020-02-16 12:31:27 -0500

    createApp component

---
+++ client/src/components/createApp.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+class createApp extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                createApp ting
+            </div>
+        );
+    }
+
+};
+
+export default createApp;
