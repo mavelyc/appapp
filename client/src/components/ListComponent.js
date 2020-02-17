ad7f59df65b9ecefb63d59af026d9c0c8f0f8941
mavelyc <cmavely@gmail.com>
2020-02-17 15:21:05 -0500

    handle status object

--- client/src/components/ListComponent.js
+++ client/src/components/ListComponent.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
 
-const ListComponent = props => (
-    <tr>
-        <td>{props.application.company}</td>
-        <td>{props.application.position}</td>
-        {/* <td>{props.application.status}</td> */}
-        <td>{props.application.date.substring(0,10)}</td>
-    </tr>
+class ListComponent extends Component {
+    constructor(props){
+        super(props)
 
-)
+        this.state = {
+            status: ""
+        }
+    }
+
+    componentDidMount() {
+        //console.log(this.props)
+        //console.log(Object.entries(this.props.application.status).filter(status => status[1] == true)[0][0])
+        this.setState({
+            status: Object.entries(this.props.application.status).filter(status => status[1] == true)[0][0]
+        })
+        
+    }
+
+    render(){
+        return(
+        <tr>
+            <td>{this.props.application.company}</td>
+            <td>{this.props.application.position}</td>
+            <td>{this.state.status}</td>
+            <td>{this.props.application.date.substring(0,10)}</td>
+        </tr>
+        );
+    }
+}
 
 export default ListComponent
